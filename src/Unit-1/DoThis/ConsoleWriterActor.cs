04f011820130edf3791d9a7c57ed78a657500507
Amen Tounou-Akue <amen@hubtel.com>
2023-08-05 22:23:21 +0000

    leson 1

--- src/Unit-1/DoThis/ConsoleWriterActor.cs
+++ src/Unit-1/DoThis/ConsoleWriterActor.cs
@@ -25,7 +25,7 @@ namespace WinTail
             // if message has even # characters, display in red; else, green
             var even = msg.Length % 2 == 0;
             var color = even ? ConsoleColor.Red : ConsoleColor.Green;
-            var alert = even ? "Your string had an even # of characters.\n" : "Your string had an odd # of characters.\n";
+            var alert = even ? "["+msg+"] has an even # of characters.\n" : "[" + msg + "] has an odd # of characters.\n";
             Console.ForegroundColor = color;
             Console.WriteLine(alert);
             Console.ResetColor();
